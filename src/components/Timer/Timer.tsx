3a159205236403e52e4622012a737b979197fb05
KonGkal <konsgkal@gmail.com>
2021-10-10 21:39:54 +0100

    add: ElapsedTime type and state

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Timer.module.scss'
 
+export type ElapsedTime = {
+  seconds: number
+  minutes: number
+  hours: number
+}
 export interface TimerProps {
   /**
    * Is this the principal call to action on the page?
@@ -27,6 +32,8 @@ export interface TimerProps {
    * Optional click handler
    */
   onClick?: () => void
+
+  elapsedTime: ElapsedTime
 }
 
 /**
@@ -38,12 +45,14 @@ export const Timer = ({
   className = '',
   backgroundColor,
   children,
+  elapsedTime,
   ...props
 }: TimerProps) => {
+  const [time, setTime] = useState(elapsedTime)
   const classes = [styles.wrapper, styles[color], className].join(' ').trim()
   return (
     <div className={classes} style={{ backgroundColor }} {...props}>
-      {children}
+      <span></span>
     </div>
   )
 }
